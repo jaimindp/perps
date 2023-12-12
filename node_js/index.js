e3e211e3c6bc585493d5ad8c683c8da33322c2ef
jaimindp <jaimin888patel@gmail.com>
2023-12-12 23:58:39 +0000

    getting the Aevo signer from ethers

--- node_js/index.js
+++ node_js/index.js
@@ -1,8 +1,18 @@
 
-const signer = ethers.Wallet.createRandom()
+const ethers = require('ethers');
+const { promisify } = require('util');
+// const provider = ethers.getDefaultProvider();
 
-// First we hash the register data
-const registerHash = ethers.utils._TypedDataEncoder.hash(
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_RPC); // Use the ALCHEMY_RPC variable from .env
+
+// Use the appropriate ethers.js methods for your operations
+
+async function main() {
+  const signer = ethers.Wallet.createRandom();
+
+  // First we hash the register data
+  const registerHash = ethers.utils._TypedDataEncoder.hash(
     {
       name: "Aevo Mainnet",
       version: "1",
@@ -18,13 +28,33 @@ const registerHash = ethers.utils._TypedDataEncoder.hash(
       key: await signer.getAddress(),
       expiry: ethers.constants.MaxUint256.toString(),
     }
-);
+  );
 
-// Then we sign the hash
-const res = await promisify(provider.provider.sendAsync)({
-  method: "eth_sign",
-  params: [account.toLowerCase(), registerHash],
-});
+  // Your existing code...
 
-// This is the account_signature
-const accountSignature = res.result;
+  // Then we sign the hash
+  const res = await promisify(provider.provider.sendAsync)({
+    method: "eth_sign",
+    params: [account.toLowerCase(), registerHash],
+  });
+
+  // This is the account_signature
+  const accountSignature = res.result;
+
+  // This is the signing_key_signature
+  const signingKeySignature = await signer._signTypedData(
+    {
+      name: "Aevo Mainnet",
+      version: "1",
+      chainId: 1,
+    },
+    {
+      SignKey: [{ name: "account", type: "address" }],
+    },
+    {
+      account: your_wallet_address,
+    }
+  );
+}
+
+main().catch(console.error);
